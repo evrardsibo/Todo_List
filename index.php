291468b682fa4bab6c6511b17d40a32630314936
evrardsibo <evrard.sibomana@gmail.com>
2022-04-20 21:40:05 +0200

    'update'

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script defer src="assets/js/app.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Square+Peg&display=swap" rel="stylesheet">
+    <title>Todo</title>
+</head>
+<body>
+    <div class="container">
+        <header>
+            <div class="logo">Evrard Todo</div>
+        </header>
+        <div class="content">
+            <div class="todo-container">
+                <h1>Ma todo</h1>
+                <div class="todo-form"></div>
+                <div class="todo-list"></div>
+            </div>
+        </div>
+        <footer>©Copyright 2021-2022 evrard.be - Tous droits réservés</footer>
+    </div>
+</body>
+</html>
